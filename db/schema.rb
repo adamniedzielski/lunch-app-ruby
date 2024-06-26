6b4afea0efff482a679404e1d4bc44636139575a
Adam Niedzielski <adamsunday@gmail.com>
2024-06-26 13:40:18 +0200

    Create Cuisine

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_06_26_113516) do
+ActiveRecord::Schema[8.0].define(version: 2024_06_26_113928) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cuisines", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "locations", force: :cascade do |t|
     t.string "name"
